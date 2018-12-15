f1789f3910a15f1fbe73170bb5e67dfb5f49b428
Yaumil Ikhsan <yaumilikhsan98@gmail.com>
2018-12-16 04:32:23 +0700

    prod 1.0

---
+++ config.js
@@ -0,0 +1,17 @@
+// =============== CREDENTIALS OLD TWITTER ACCOUNT ==================
+// module.exports = {
+//   consumer_key:         'F6QkKJrQhmnyJzvO3RfuUhXx7',
+//   consumer_secret:      'Pq51dlHHt0PbQYiM357057c4wHaLUtTo4N12aSXyV47Vzr2IsT',
+//   access_token:         '1071345885076893696-LXEmw62RsFBTcq5uLXmpKvtuUOrrIu',
+//   access_token_secret:  'hyqXwG0TIIfTHmPKRWnfQI3f5tkM07AP8Ixi74l8Y7RXK',
+//   timeout_ms:           60 * 1000,
+// };
+
+// =============== CREDENTIALS NEW TWITTER ACCOUNT ==================
+module.exports = {
+  consumer_key:         'dFbQ6o2DnhHIoawGY2Xj5IJuw',
+  consumer_secret:      'G7W6yuZFCuU0FhFeMkH4m6lz6UBRmSyJz1FWcxi8KgnqHyaq0j',
+  access_token:         '1072145298825170944-I2vYDvPN2Uwbq4hkpNED8neMhf3q8W',
+  access_token_secret:  'W91TPCrGLfeGrQZnu2b80i0RgmQDiNcArWA9C8tIQKNib',
+  timeout_ms:           60 * 1000
+}
