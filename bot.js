7401dc9d2368ee3cd7e1405f5cb3280e7856ab38
Yaumil Ikhsan <yaumilikhsan98@gmail.com>
2018-12-18 03:28:45 +0700

    small refactot

--- bot.js
+++ bot.js
@@ -5,125 +5,124 @@ const Twit = require('twit');
 const Jimp = require('jimp'); 
 const download = require('image-downloader');
 const fs = require('fs');
-const file = require('file-system');
 
-const T = new Twit(config);
+const T = new Twit(config.cred);
 
 const onAuthenticated = (err, res) => {
-  if (err) throw err;
-  console.log('Authentication successful. Running bot...\r\n');
+	if (err) throw err;
+
+	console.log('Authentication successful. Running...\r\n');
 };
 
 T.get('account/verify_credentials', {
-  include_entities: false,
-  skip_status: true,
-  include_email: false
+	include_entities: false,
+	skip_status: true,
+	include_email: false
 }, onAuthenticated);
 
 const getData = async (id) => {
-  const res = await axios(`https://www.bacaquran.online/api/ayat/${id}`);    
-
-  return res.data;
+	const res = await axios(`${config.url.textAPI}/${id}`);
+	console.log('\r\ngot data from the API');
+	return res.data;
 };
 
 const getAyat = (data) => {
-  const ayat =`${data.terjemahan_idn} (${data.surat.nama_surat}:${data.nomor_ayat})`;
-  return ayat;
+	const ayat =`${data.terjemahan_idn} (${data.surat.nama_surat}:${data.nomor_ayat})`;
+	console.log('data was edited as ayat');
+	return ayat;
 };
 
 const downloadImage = async() => {
-  options = {
-    url: 'https://source.unsplash.com/1200x1200/?islam,mosque,quran',
-    dest: 'raw/photo.jpg' 
-  };
+	options = {
+		url: config.url.imageAPI,
+		dest: 'raw/photo.jpg' 
+	};
 
-  try {
-    const { filename, image } = await download.image(options);
-    console.log(`File saved tp ${ filename }`)
-    
-  } catch (error) {
-    console.log(error);
-  };   
+	try {
+		const { filename, image } = await download.image(options);
+		console.log(`image downloaded, file saved to ${ filename }`);
+	} catch (error) {
+		console.log(error);
+	};   
 };
 
 const editImage = async (text) => {
-  try {
-    const font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
-    console.log(`Font loaded`);
+	try {
+		const font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+		console.log(`font loaded`);
 
-    const image = await Jimp.read('raw/photo.jpg');
-    console.log(`image loaded`);
+		const image = await Jimp.read('raw/photo.jpg');
+		console.log(`image ready to edit`);
 
-    image.brightness( -0.6 )
-    .print(
-      font,
-      200,
-      200,
-      {
-        text: text,
-        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-        alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
-      },
-      800,
-      800
-    )
-    .write('export/photo.jpg');
-    console.log(`image printed`);
-  } catch (error) {
-    console.log(error);
-  }
+		image.brightness(-0.6).print(
+			font,
+			200,
+			200,
+			{
+				text: text,
+				alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+				alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
+			},
+			800,
+			800
+		)
+		.write('export/photo.jpg');
+		console.log(`Image edited`);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 const tweetIt = async() => {
 
-  var b64content = fs.readFileSync('export/photo.jpg', { encoding: 'base64' });
+	var b64content = fs.readFileSync('export/photo.jpg', { encoding: 'base64' });
 
-  console.log('Uploading an image...');
+	console.log('Uploading an image...');
 
-  T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-    if (!err){
-      console.log('Image uploaded!');
-      console.log('Now tweeting it...');
+	T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+		if (err) throw err;
 
-      var mediaIdStr = data.media_id_string;
-      var params = {
-        media_ids: [mediaIdStr]
-      }
+		console.log('Image uploaded!');
+		console.log('Now tweeting it...');
 
-      T.post('statuses/update', params, function(err, data, response) {
-        if (err){
-          console.log('ERROR:');
-          console.log(err);
-        }
-        else{
-          console.log('Posted an image!');
-        }
-      });
-    }
-    else{
-      console.log('ERROR:');
-      console.log(err);
-    }
-  });
+		var mediaIdStr = data.media_id_string;
+		var params = {
+			media_ids: [mediaIdStr]
+		}
+
+		T.post('statuses/update', params, function(err, data, response) {
+			if (err) throw err;
+			else console.log('Posted an image!');
+		});
+
+	});
 };
 
 const controlTweetImage = async () => {
-  const ran = Math.floor(Math.random() * (6223 - 1 + 1)) + 1;
+	// create a rundom number in range 1 - 6223, 
+	const ran = Math.floor(Math.random() * (6223 - 1 + 1)) + 1;
 
-  const data = await getData(ran);
+	// get data from the api call
+	const data = await getData(ran);
 
-  const ayat = getAyat(data);
+	// get ayat processed from the API given data
+	const ayat = getAyat(data);
 
-  if (ayat.length < 240 ) {
- 
-    await downloadImage();
-  
-    editImage(ayat);
+	// checking if the ayat not more than 240 char
+	// for prevent  the text out of the image size
+	if (ayat.length < 240 ) {
+	
+		// get the image from the web image public API
+		await downloadImage();
+	
+		// adding text (in this case is an ayat) to the image
+		editImage(ayat);
+		
+		// Tweet and publish the image who has edited to Twitter
+		tweetIt();
+	}
 
-    tweetIt();
-  }
-
-  else console.log(`text lebih dari 240 karakter`);
+	else console.log(`text lebih dari 240 karakter`);
 };
 
-setInterval(controlTweetImage, 3600000);
+setInterval(controlTweetImage, 30000);
