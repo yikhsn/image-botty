d298d27b8968ece03326cdd6e207ecd504b91bee
Yaumil Ikhsan <yaumilikhsan98@gmail.com>
2018-12-24 03:35:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const cred = {
-  consumer_key:         'dFbQ6o2DnhHIoawGY2Xj5IJuw',
-  consumer_secret:      'G7W6yuZFCuU0FhFeMkH4m6lz6UBRmSyJz1FWcxi8KgnqHyaq0j',
-  access_token:         '1072145298825170944-I2vYDvPN2Uwbq4hkpNED8neMhf3q8W',
-  access_token_secret:  'W91TPCrGLfeGrQZnu2b80i0RgmQDiNcArWA9C8tIQKNib',
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
   timeout_ms:           60 * 1000
 }
 
 const url = {
-  textAPI:    'https://www.bacaquran.online/api/ayat',
-  imageAPI:   'https://source.unsplash.com/1200x1200/?islam,mosque,quran'
+  textAPI:    '',
+  imageAPI:   ''
 }
 
 module.exports.cred = cred;
